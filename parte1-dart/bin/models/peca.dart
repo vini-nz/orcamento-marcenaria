94e5b535db94811303b265c553883fdb0c13f2a6
vini-nz <viniciusbsantos07@gmail.com>
2026-09-07 18:05:12 -0300

    Adiciona a classe Peca com construtor nomeado e getters de area e custo

---
+++ parte1-dart/bin/models/peca.dart
@@ -0,0 +1,27 @@
+class Peca {
+  Peca({
+    required this.descricao,
+    required this.largura,
+    required this.altura,
+    required this.quantidade,
+    required this.medidaEm,
+    this.material = 'MDF 15mm',
+    this.precoMetroQuadrado = 320.00,
+  });
+
+  final String descricao;
+  final double largura;
+  final double altura;
+  final int quantidade;
+  final DateTime medidaEm;
+  final String material;
+  final double precoMetroQuadrado;
+
+  double get area => largura * altura * quantidade;
+
+  double get custo => area * precoMetroQuadrado;
+
+  String ficha() =>
+      '$descricao, ${largura.toStringAsFixed(2)}x${altura.toStringAsFixed(2)}m, '
+      'qtd $quantidade, $material';
+}
